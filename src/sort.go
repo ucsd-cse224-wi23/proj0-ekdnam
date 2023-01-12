53327699b3a2f3b222446b48ecbafbe257ed0776
Aditya Mandke <40426312+ekdnam@users.noreply.github.com>
2023-01-11 21:07:41 -0800

    sort file updated - read file in chunks

--- src/sort.go
+++ src/sort.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+type ByRecord []Record
+
+type Record struct {
+	Key   byte
+	Value byte
+}
+
+func (a ByRecord) Len() int           { return len(a) }
+func (a ByRecord) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByRecord) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -12,5 +25,42 @@ func main() {
 		log.Fatalf("Usage: %v inputfile outputfile\n", os.Args[0])
 	}
 
+	const BufferSize = 100
+	const KeySize = 10
+	const ValueSize = 90
+
+	file, err := os.Open(os.Args[0])
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	buffer := make([]byte, BufferSize)
+	bufferKey := make([]byte, KeySize)
+	bufferValue := make([]byte, ValueSize)
+
+	records := make([]Record, 0)
+
+	for {
+		bytesKey, errKey := file.Read(bufferKey)
+		bytesValue, errValue := file.Read(bufferValue)
+
+		if errKey != nil {
+			if errKey != io.EOF {
+				fmt.Println(errKey)
+			}
+
+			break
+		}
+		if errValue != nil {
+			if errValue != io.EOF {
+				fmt.Println(errValue)
+			}
+
+			break
+		}
+
+		records = append(records, Record{bytesKey, bytesValue})
+	}
 	log.Printf("Sorting %s to %s\n", os.Args[1], os.Args[2])
 }
